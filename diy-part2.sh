efd63ca35f51e5241b8887f343d919e4f7e29f35
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2022-01-26 17:10:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,8 +23,8 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-s
 git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
 
 # 获取hello world和依赖
-git clone https://github.com/jerrykuku/lua-maxminddb package/helloworld/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr package/helloworld/luci-app-vssr
+# git clone https://github.com/jerrykuku/lua-maxminddb package/helloworld/lua-maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr package/helloworld/luci-app-vssr
 
 # 获取主题
 git clone https://github.com/Kinodi/luci-theme-atmaterial package/luci-theme-atmaterial
@@ -35,7 +35,7 @@ git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
 # 获取 openwrt-bypass
-git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
+# git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
 
 # 获取 openclash
 # git clone https://github.com/vernesong/OpenClash package/luci-app-OpenClash
