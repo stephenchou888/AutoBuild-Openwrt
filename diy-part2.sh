eaec272f1894b9741a6cd2c64193fde679746bc2
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2022-03-06 07:29:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,17 +14,17 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 获取luci-app-adguardhome
-# git clone https://github.com/uamrws/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/uamrws/luci-app-adguardhome package/luci-app-adguardhome
 
 # 获取luci-app-smartdns
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 
 # 获取luci-app-dnsfilter
 git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
 
 # 获取hello world和依赖
-# git clone https://github.com/jerrykuku/lua-maxminddb package/helloworld/lua-maxminddb
-# git clone https://github.com/jerrykuku/luci-app-vssr package/helloworld/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb package/helloworld/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/helloworld/luci-app-vssr
 
 # 获取主题
 git clone https://github.com/Kinodi/luci-theme-atmaterial package/luci-theme-atmaterial
@@ -35,10 +35,10 @@ git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
 # 获取 openwrt-bypass
-# git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
+git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
 
 # 获取 openclash
-# git clone https://github.com/vernesong/OpenClash package/luci-app-OpenClash
+git clone https://github.com/vernesong/OpenClash package/luci-app-OpenClash
 
 # 获取 netdata
 git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
