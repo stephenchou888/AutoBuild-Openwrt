a6380418ea24aab8ed00910b493ff15120ba1a72
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2022-03-08 05:38:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -35,7 +35,7 @@ git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
 # 获取 openwrt-bypass
-# git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
+git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
 
 # 获取 openclash
 # git clone https://github.com/vernesong/OpenClash package/luci-app-OpenClash
